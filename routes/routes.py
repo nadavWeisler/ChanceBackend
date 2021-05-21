f42eb206de9e03e01a858656173b18567ffa0e0b
ilanerukh <ilanerukh@gmail.com>
2021-05-21 04:46:01 +0300

    changed routs

--- routes/routes.py
+++ routes/routes.py
@@ -11,9 +11,8 @@ def initialize_routes(api):
     api.add_resource(LoginApi, '/api/auth/login')
     api.add_resource(SearchEngine, '/api/search')
     api.add_resource(CompanyPersonalSpace, '/api/personalSpace/company')
-    api.add_resource(CompanyPersonalSpace, '/api/personalSpace/student')
+    api.add_resource(StudentPersonalSpace, '/api/personalSpace/student')
 
     api.add_resource(ApproveCandidate, '/api/personalSpace/approveRequest')
     api.add_resource(OfferCandidate, '/api/personalSpace/setRequest')
     api.add_resource(FinishInternship, '/api/personalSpace/finishProject')
-    api.add_resource(Person)
